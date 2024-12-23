610a4e5deb40dc496caaae15c743a049a7828745
TimLi <daochouwangu@gmail.com>
2024-12-23 15:26:35 +0800

    测试

--- app/components/EditorBar.tsx
+++ app/components/EditorBar.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { TranslateButton } from "./bar/TranslateButton";
 import { CopyToWechatButton } from "./bar/CopyToWechatButton";
+import { TestImageButton } from "./bar/TestImageButton";
+
 export const EditorBar = () => {
   return (
-    <div className="flex flex-row gap-1 h-8">
+    <div className="flex gap-2 p-2">
       <TranslateButton />
       <CopyToWechatButton />
+      <TestImageButton />
     </div>
   );
 };
